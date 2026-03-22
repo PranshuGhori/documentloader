3963277d23bd0bfc00aa9062318539e14019eb39
PranshuGhori <pranshughori137@gmail.com>
2026-03-21 21:10:24 -0700

    Splitter

--- document loaders/pdf.py
+++ document loaders/pdf.py
@@ -1,7 +1,13 @@
 from langchain_community.document_loaders import PyPDFLoader
-
+from langchain_text_splitters import TokenTextSplitter
 
 
 data = PyPDFLoader("document loaders/ps1.pdf")
 documents = data.load()
-print(len(documents))
+splitter = TokenTextSplitter(
+    model_name="gpt-4",
+    chunk_size=1000,
+    chunk_overlap=10
+)
+chunk = splitter.split_documents(documents)
+print(chunk[0].page_content)
--- document loaders/test.py
+++ document loaders/test.py
@@ -1,8 +1,20 @@
 import os
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 loader = TextLoader(os.path.join(current_dir, "notes.txt"))
 
+splitter = CharacterTextSplitter(separator="",
+    chunk_size=10,
+    chunk_overlap=1
+)
+
 documents = loader.load()
-print(documents)
+
+print(len(splitter.split_documents(documents)))
+
+for i in splitter.split_documents(documents):
+    print(i.page_content)
+    print("\n")
